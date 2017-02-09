54c0cb21148fcb306ae227580be6f70532ce5a53
Ieva <Ieva>
2017-02-09 12:12:53 +0000

    Chapter 6 exercises and Chapter 7 complete

--- populate_rango.py
+++ populate_rango.py
@@ -11,25 +11,25 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"},
+         "url":"http://docs.python.org/2/tutorial/", "views": 32},
         {"title": "How to Think like a Computer Scientist",
-         "url": "http://www.greenteapress.com/thinkpython/"},
+         "url": "http://www.greenteapress.com/thinkpython/", "views": 7},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"} ]
+         "url": "http://www.korokithakis.net/tutorials/python/", "views": 15} ]
 
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial101/"},
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial101/", "views": 20},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/", "views": 40},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"} ]
+         "url": "http://www.tangowithdjango.com/", "views": 17} ]
 
     other_pages = [
         {"title": "Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/", "views": 4},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org/"} ]
+         "url": "http://flask.pocoo.org/", "views": 8} ]
 
     cats = {"Python": {"pages": python_pages, "likes": 64, "views": 128},
             "Django": {"pages": django_pages, "likes": 32, "views": 64},
@@ -38,13 +38,13 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["likes"], cat_data["views"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title) [0]
     p.url=url
     p.views=views
--- rango/views.py
+++ rango/views.py
@@ -2,16 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
@@ -31,3 +30,29 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But since the most recent category added is on the index page
+            # Then we can direct the user back to the index page.
+            return index(request)
+        else:
+            # The supplied form contained errors
+            # just print them to the terminal.
+            print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
